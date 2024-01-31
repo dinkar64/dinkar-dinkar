0fab5c6460d4a4827482341953ca3fd074a53074
dinkar64 <dinkarrathi64@gmail.com>
2024-01-30 16:53:04 -0800

    Correction - 1

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,24 +2,25 @@
 
 import React, { useEffect } from 'react';
 import profileImage from './profile_img.jpg'; // Replace with your actual image file name
+import './Home.css';
 
 const Home = () => {
   useEffect(() => {
     document.title = 'Dinkar';
   }, []);
 
-  const linkedinUrl = 'https://www.linkedin.com/in/dinkar-dinkar'; // Replace with your LinkedIn profile URL
+  const linkedinUrl = 'https://www.linkedin.com/in/dinkar-dinkar';
 
   return (
-    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+    <div className="home-container">
       <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
         <img
           src={profileImage}
           alt="Profile"
-          style={{ borderRadius: '50%', width: '250px', height: '250px', marginTop: '160px' }}
+          className="profile-image"
         />
       </a>
-      <p style={{ fontStyle: 'italic', fontSize: '18px', marginTop: '20px' }}>
+      <p className="tagline">
         Curious problem-solver in the dynamic realm of technology.
       </p>
     </div>
